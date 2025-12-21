640b331f15756f5324a55d94ad199d44b2c5b4e5
Prathamjot Singh <66751293+Prathamjot03@users.noreply.github.com>
2025-12-22 01:02:30 +0530

    Update YouTube API key in index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
     fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
-    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyBUECysApSssdoi4DMD8lT41TgMz8mr92g`
+    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=Your_Youtube_Api_Key`
 }
 
 
